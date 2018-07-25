22e5723c24505e50f479bbcac6ddc7a1f4e2e7db
Javier Gomez <javier.gomez.p@ug.uchile.cl>
2018-07-25 12:53:47 -0400

    register models

--- logistica/admin.py
+++ logistica/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(HorarioDisponible)
+admin.site.register(Espacio)
+admin.site.register(Monitor)
+admin.site.register(Disponibilidad)
+admin.site.register(Charla)
