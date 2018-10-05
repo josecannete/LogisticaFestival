4b842b9c37ced60727cea9744201eb6d96360f6f
José Cañete <joserodolfocl@gmail.com>
2018-10-05 15:45:44 -0300

    updated models

--- logistica/models.py
+++ logistica/models.py
@@ -83,11 +83,9 @@ class Tour(models.Model):
     duracion = models.IntegerField()
     alumnos = models.IntegerField()
     visitas = models.ManyToManyField(Visita)
-    confirmado = models.BooleanField # False si es opcion, True si esta confirmado
 
     def __str__(self):
         return str(self.nombre)
-
 
 
 # Actividad
@@ -104,3 +102,28 @@ class Actividad(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+'''
+
+Todo lo que esta abajo es para no tener que guardar los posibles tours en la base de datos que consultamos
+
+'''
+
+# PosibleTour
+class PosibleTour(models.Model):
+    nombre = models.CharField(max_length=200)
+    monitor = models.ForeignKey(Monitor, on_delete=models.CASCADE, null=True)
+    horaInicio = models.TimeField()
+    duracion = models.IntegerField()
+    alumnos = models.IntegerField()
+    visitas = models.ManyToManyField(Visita)
+
+    def __str__(self):
+        return str(self.nombre)
+
+# PosibleVisita
+# Un tour es un conjunto de visitas
+class PosibleVisita(models.Model):
+    horario = models.ManyToManyField(Horario)
+    espacio = models.ForeignKey(Espacio, on_delete=models.CASCADE)
