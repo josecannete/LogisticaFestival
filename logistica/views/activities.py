8fa4eb8ff8d6f42001e13dbdcc704fd0230e3422
Franco Sepúlveda <franco.sepulveda.p@gmail.com>
2018-10-10 14:57:28 -0300

    Fix'd error after removed option to change monitor

--- logistica/views/activities.py
+++ logistica/views/activities.py
@@ -31,7 +31,6 @@ def generate_activity_id_list(user=None):
 def edit_activity_capacity(request):
     pk_activity = request.POST.get("id")
     ca_activity = int(request.POST.get("capacidadActual"))
-    me_activity = int(request.POST.get("monitorEncargado"))
     ans = {
         'status': False,
         'msg': ''
@@ -46,18 +45,12 @@ def edit_activity_capacity(request):
         ans['msg'] = 'Actividad con dicho ID no existe'
         return JsonResponse(ans)
 
-    monitor = Monitor.objects.filter(pk=me_activity)
-    if not monitor.exists():
-        ans['msg'] = 'Monitor con dicho ID no existe'
-        return JsonResponse(ans)
-
     activity = Actividad.objects.get(pk=pk_activity)
     if activity.capacidadTotal < ca_activity:
         ans['msg'] = 'No puede existir sobrecapacidad!'
         return JsonResponse(ans)
 
     activity.capacidadActual = ca_activity
-    activity.monitor = Monitor.objects.get(pk=me_activity)
     activity.save()
     ans['status'] = True
     return JsonResponse(ans)
