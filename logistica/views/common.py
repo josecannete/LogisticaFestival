e45cedea9bf245dc121060a18de1d452805d45fa
constanza fierro <constanza.fierro94@gmail.com>
2018-10-18 05:13:15 -0300

    filter charlas/talleres less than two hours ahead

--- logistica/views/common.py
+++ logistica/views/common.py
@@ -151,8 +151,10 @@ def principal(request):
         # start_time = datetime.datetime.now()
         start_time = timezone.now().replace(day=18, hour=9)  # TODO: Eliminar
         charlas = Actividad.objects.filter(horario__inicio__gt=start_time,
+                                           horario__inicio__lt=(start_time + datetime.timedelta(hours=2)),
                                            horario__inicio__day=start_time.day, tipo='charla').order_by('horario__inicio')
         talleres = Actividad.objects.filter(horario__inicio__gt=start_time,
+                                            horario__inicio__lt=(start_time + datetime.timedelta(hours=2)),
                                             horario__inicio__day=start_time.day, tipo='taller').order_by('horario__inicio')
         context = {
             'charlas': charlas,
